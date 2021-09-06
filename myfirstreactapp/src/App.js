cbd2d079598696f05e50d01db0bd77c4a932879f
Afeez <aoladokun@my.apsu.edu>
2021-09-05 23:00:28 -0600

    myFirstReactApp

---
+++ myfirstreactapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Hello Dojo!</h2>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
