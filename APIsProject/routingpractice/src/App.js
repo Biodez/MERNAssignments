f2afcde40ae4b9692fd8c633eda26f3dc0c40639
Afeez <aoladokun@my.apsu.edu>
2021-09-26 22:13:05 -0400

    adding routing practice project

---
+++ APIsProject/routingpractice/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import { Router } from "@reach/router";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        <Home path="/home" />
+        <Home path="/:word" />
+        <Home path="/:word/:font/:bground" />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
