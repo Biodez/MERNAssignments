00df304024af249d3ffe65e67b58d199d3f083c4
Afeez <aoladokun@my.apsu.edu>
2021-09-27 00:36:50 -0400

    fakerAPI assignment

---
+++ faker_api/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const faker = require("faker");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const createUser = () => {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.phoneNumber(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+};
+
+const createCompany = () => {
+  return {
+    companyName: faker.company.companyName(),
+    department: faker.commerce.department(),
+    address: {
+      streetAddress: faker.address.streetAddress(),
+      city: faker.address.city(),
+      state: faker.address.state(),
+      zip: faker.address.zipCode(),
+      country: faker.address.country(),
+    },
+  };
+};
+
+app.get("/api/users/new", (request, response) => {
+  const newUser = createUser();
+  console.log("new user:");
+  console.log(newUser);
+  response.json(newUser);
+});
+
+app.get("/api/companies/new", (request, response) => {
+  let newCompany = createCompany();
+
+  console.log("new company:");
+  console.log(newCompany);
+  newCompany.department = faker.commerce.department();
+  console.log("updated company department:");
+  console.log(newCompany);
+
+  response.json(newCompany);
+});
+
+app.get("/api/user/company", (request, response) => {
+  let newUser = createUser();
+  let newCompany = createCompany();
+
+  console.log("new user:");
+  console.log(newUser);
+
+  console.log("new company:");
+  console.log(newCompany);
+  response.json({ user: newUser, company: newCompany });
+});
+
+app.listen(9000, () => console.log("Listening on port 9000"));
