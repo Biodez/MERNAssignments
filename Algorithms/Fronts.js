e99621e09e63e82650bf1b3a2ee7afe8388176da
Afeez <aoladokun@my.apsu.edu>
2021-11-07 18:55:40 -0500

    another commit

--- Algorithms/Fronts.js
+++ Algorithms/Fronts.js
@@ -1,7 +1,34 @@
-class SLL {
-    // constructor, other methods, removed for brevity
-    addFront(value) {
-       
-   }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
+class SLL {
+  // constructor, other methods, removed for brevity
+  constructor() {
+    this.head = null;
+  }
+  addFront(value) {
+    var newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    return this;
+  }
+
+  removeFront() {
+    if (this.head === null) {
+      return this.head;
+    }
+
+    var valToRemove = this.head;
+    this.head = valToRemove.next;
+    valToRemove.next = null;
+    return this.head;
+  }
+
+  front() {
+    return this.head === null ? null : this.head.value;
+  }
+}
